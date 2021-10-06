28d54aa2585809c4bc0708a40a873c335af3bab3
mahfuz60 <mahfuzuralam1996@gmail.com>
2021-10-06 14:35:43 +0600

    code

--- index.js
+++ index.js
@@ -3,15 +3,46 @@ const express = require("express");
 const multer = require("multer");
 
 //preparing upload folder
-const UPLOADS_FOLDER = ("/uploads");
+const UPLOADS_FOLDER = "./uploads/";
 var upload = multer({
   dest: UPLOADS_FOLDER,
+  limits: {
+    fileSize: 1000000, //1MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image.jpg" ||
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("only jpg,png and jpeg file are allowed!"));
+    }
+  },
 });
 
 const app = express();
-
+//single file upload
 app.post("/", upload.single("avatar"), (req, res) => {
-  res.send("Hello, world!");
+  res.send("File uploaded successfully done!");
+});
+//multiple file upload
+// app.post("/", upload.array("avatar", 5), (req, res) => {
+//   res.send("File uploaded successfully done!");
+// });
+
+//default error handlers
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).send("there was a upload error!");
+    } else {
+      res.status(500).send(err.message);
+    }
+  } else {
+    res.send("successfully uploaded");
+  }
 });
 
 app.listen(3000, () => {
